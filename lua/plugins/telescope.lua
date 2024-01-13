f3f9a389c18513e5f0c1afee430a6ed07089b0cf
florian.laine <florian@ynstant.io>
2024-01-13 18:03:47 +0100

    Working man TODO and conflict

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,11 @@ return {
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
 		{ "<leader>fc", "<cmd>Telescope grep_string search=<<<<<<CR>", desc = "Find merge conflict" },
+		{ "<leader>ft", "<cmd>Telescope grep_string search=TODO<CR>", desc = "Find TODOs" },
 		{ "<leader>fa", "<cmd>Telescope find_files hidden=true no_ignore=true<CR>", desc = "Find all" },
 		{ "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Find keys" },
 		{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
 		{ "<leader>fw", "<cmd>Telescope live_grep<CR>", desc = "Find in files" },
-		{ "<leader>fm", "<cmd>Telescope man_pages section={'ALL'}", desc = "Man page" },
+		{ "<leader>fm", "<cmd>Telescope man_pages section={'ALL'}<CR>", desc = "Man page" },
 	},
 }
