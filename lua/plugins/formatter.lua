5f7e4afa787a3b5a1c8ad5afabe9f5038e319180
tim-pearson <timothy-edward.pearson@epita.fr>
2025-05-20 14:19:18 +0200

    py formatter 79 max chars per line

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,18 +1,46 @@
+-- return {
+--     "mhartington/formatter.nvim",
+--     config = function()
+--         local types = "formatter.filetypes."
+--         require("formatter").setup({
+--             filetype = {
+--                 python = {
+--                     require(types .. "python").black,
+--                     -- require(types .. "python").isort,
+--                 },
+--             },
+--         })
+
+--         -- Keybinding for formatting
+--         vim.api.nvim_set_keymap("n", "<leader>f", ":silent! Format<CR>", { noremap = true, silent = true })
+--     end,
+-- }
+
 return {
     "mhartington/formatter.nvim",
     config = function()
-        local types = "formatter.filetypes."
         require("formatter").setup({
             filetype = {
                 python = {
-                    require(types .. "python").black,
-                    -- require(types .. "python").isort,
+                    function()
+                        return {
+                            exe = "black",
+                            args = { "--line-length", "79", "-" },
+                            stdin = true,
+                        }
+                    end,
+                    -- You can also enable isort here if desired:
+                    -- require("formatter.filetypes.python").isort,
                 },
             },
         })
 
         -- Keybinding for formatting
-        vim.api.nvim_set_keymap("n", "<leader>f", ":silent! Format<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>f",
+            ":silent! Format<CR>",
+            { noremap = true, silent = true }
+        )
     end,
 }
-
