47f6d0cfa593db8f5122f4352b2526a773391cb6
florian.laine <florian.laine@epita.fr>
2025-07-06 03:38:10 +0200

    fix: changed cmake

--- snippets/cmake.lua
+++ snippets/cmake.lua
@@ -1,24 +1,24 @@
 return {
-  s("cmake", {
-    t({
-      "cmake_minimum_required(VERSION 3.0)",
-      "project(",
-    }),
-    i(1, "PROJECT"),
-    t({
-      ")",
-      "file(GLOB_RECURSE sources src/*.cpp)",
-      "",
-      "add_executable(",
-    }),
-    l(l._1, 1),
-    t({
-      " ${sources})",
-      "target_compile_options(",
-    }),
-    l(l._1, 1),
-    t({
-      " PUBLIC -Wall -Wextra -Werror -pedantic -std=c++20 -Wold-style-cast -g -fsanitize=address -Wno-error=old-style-cast)",
-    }),
-  }),
+	s("cmake", {
+		t({
+			"cmake_minimum_required(VERSION 3.5)",
+			"project(",
+		}),
+		i(1, "PROJECT"),
+		t({
+			")",
+			"file(GLOB_RECURSE sources src/*.cpp)",
+			"",
+			"add_executable(",
+		}),
+		l(l._1, 1),
+		t({
+			" ${sources})",
+			"target_compile_options(",
+		}),
+		l(l._1, 1),
+		t({
+			" PUBLIC -Wall -Wextra -Werror -pedantic -std=c++20)",
+		}),
+	}),
 }
