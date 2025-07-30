6c6804651f12c1332ab8d996ecc0f4b4770a18ef
florian.laine <florian.laine@epita.fr>
2025-07-30 05:10:47 +0200

    current config

--- lua/plugins/cpp-tools.lua
+++ lua/plugins/cpp-tools.lua
@@ -1,35 +1,35 @@
 return {
-    -- dir = "~/Code/cpp-tools.nvim",
-    "Frayzen/cpp-tools.nvim",
-    config = function()
-        require("cpp-tools").setup()
-        require("which-key").add({
-            { "<Leader>t", group = "tools" },
-            {
-                "<Leader>i",
-                function()
-                    require("cpp-tools").implement()
-                end,
-                desc = "Implement",
-            },
-            {
-                "<Leader>c",
-                function()
-                    require("cpp-tools").create()
-                end,
-                desc = "Create",
-            },
-            {
-                "<Leader>r",
-                function()
-                    require("cpp-tools").refactor()
-                end,
-                desc = "Refactor",
-            },
-        })
-    end,
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-lua/plenary.nvim",
-    },
+	dir = "~/Code/cpp-tools.nvim",
+	-- "Frayzen/cpp-tools.nvim",
+	config = function()
+		require("cpp-tools").setup()
+		require("which-key").add({
+			{ "<Leader>t", group = "tools" },
+			{
+				"<Leader>ti",
+				function()
+					require("cpp-tools").implement()
+				end,
+				desc = "Implement",
+			},
+			{
+				"<Leader>tc",
+				function()
+					require("cpp-tools").create()
+				end,
+				desc = "Create",
+			},
+			{
+				"<Leader>tr",
+				function()
+					require("cpp-tools").refactor()
+				end,
+				desc = "Refactor",
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+	},
 }
