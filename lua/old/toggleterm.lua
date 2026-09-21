0dd626191a810a0e7e98c5d8d0fad2b61ac50433
frayzen <florian.laine@epita.fr>
2026-09-21 16:38:20 +0200

    feat: toggleterm

--- lua/old/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = true,
-    keys = {
-        {"<M-i>", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float", mode = { "t" , "n"}},
-    }
-}
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = true,
+  keys = {
+    { "<M-i>", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float", mode = { "t", "n" } },
+  }
+}
